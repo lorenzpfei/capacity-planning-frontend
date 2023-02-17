84914adfd07b13aaa81e6d41a6cc3df07190bb61
Lorenz Pfeifer <pfeifer.lorenz@gmail.com>
2023-02-17 01:22:08 +0100

    ✨ middleware working correctly

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,5 @@
-import axios from 'axios';
 
 export default function Home() {
-    const instance = axios.create({
-        withCredentials: true,
-        baseURL: 'http://localhost:8000/api'
-    })
-    instance.get('/user', {
-        // @ts-ignore
-        on401: (destroySession) => {
-            console.log('destroy'); //todo: remove debug
-            destroySession(); // Destroy the session and redirect to login
-        }
-    }).then((res) => {
-        console.log(res); //todo: remove debug
-    })
-
     return (
         <>
             content
